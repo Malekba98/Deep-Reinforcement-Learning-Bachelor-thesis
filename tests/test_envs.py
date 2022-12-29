ab3c0574a04ef62e4985946ed6c023429c2dce58
Malek Ben Alaya <121289306+Malekba98@users.noreply.github.com>
2022-12-29 12:32:21 +0100

    Update test_envs.py

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) 2022 Sven Gronauer (Technical University of Munich)
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
 """Check that an environment follows Gym API.
 
 Some functions are from:
