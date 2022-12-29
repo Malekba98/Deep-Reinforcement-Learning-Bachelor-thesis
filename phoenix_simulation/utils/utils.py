9e6645beac954a80cf16045a58b8d4b1b613c38b
Malek Ben Alaya <121289306+Malekba98@users.noreply.github.com>
2022-12-29 12:31:26 +0100

    Update utils.py

--- phoenix_simulation/utils/utils.py
+++ phoenix_simulation/utils/utils.py
@@ -1,3 +1,9 @@
+"""
+Copyright (c) 2022 Sven Gronauer (Technical University of Munich)
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
 r"""Utility functions for Project Phoenix-Simulation.
 
     Author:     Sven Gronauer
