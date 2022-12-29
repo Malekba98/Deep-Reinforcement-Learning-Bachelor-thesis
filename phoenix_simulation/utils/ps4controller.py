0a4059fa420f343174815337290d010a699bf0f5
Malek Ben Alaya <121289306+Malekba98@users.noreply.github.com>
2022-12-29 12:30:40 +0100

    Update ps4controller.py

--- phoenix_simulation/utils/ps4controller.py
+++ phoenix_simulation/utils/ps4controller.py
@@ -1,3 +1,9 @@
+"""
+Copyright (c) 2022 Sven Gronauer (Technical University of Munich)
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
